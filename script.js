49f0c2918c0cd6a392db2386516dfd3d98fbeb63
Luke Sturgill <lukesturgill@gmail.com>
2022-04-17 23:55:08 -0600

    added local storage saving to script.js

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
 var saveBtn = $(".saveBtn");
 
 // Display the current day at the top of page 
-$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+var todaysDate = moment().format('dddd, MMM Do YYYY');
+$("#todaysDay").html(todaysDate);
+
+// Adds color to blocks depending on hour of day and css class properties
+function containerColor() {
+    var time = moment().hours();
+    $(".hourBlocks").each(function() {
+        var currentHour = parseInt($(this).attr("id"));
+        if (currentHour > time) {
+            $(this).addClass("future");
+        } else if (currentHour === time) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+
+saveBtn.on("click", function() {
+
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".plan").val();
+
+    localStorage.setItem(time, plan);
+});
+
+//saves planned out day after refreshing page
+function savedPlans() {
+
+    $(".hour").each(function() {
+        var currentHour = $(this).text();
+        var currPlan = localStorage.getItem(currentHour);
+        
+        if(currPlan !== null) {
+            $(this).siblings(".plan").val(currPlan);
+        }
+    });
+}
+
+containerColor();
+savedPlans();
